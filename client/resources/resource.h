96bb0c2fb3d480fb96aa6ff000246fb3ce754753
zDestinate <EA.ADM1N.one@gmail.com>
2021-11-27 17:07:54 -0500

    Updated resources

--- client/resources/resource.h
+++ client/resources/resource.h
@@ -1,7 +1,8 @@
-#define IDR_FONTFA_LIGHT_300            101
-#define IDR_FONTFA_BRANDS_400           102
-#define IDR_FONTFA_REGULAR_400          103
-#define IDR_FONTFA_SOLID_900            104
-#define IDR_FONTFA_DUOTONE_900          105
+#define IDR_FONTFA_BRANDS_400           101
+#define IDR_FONTFA_DUOTONESOLID_900     102
+#define IDR_FONTFA_LIGHT_300            103
+#define IDR_FONTFA_REGULAR_400          104
+#define IDR_FONTFA_SOLID_900            105
+#define IDR_FONTFA_THIN_100             106
 
 #define IDB_APPICON                     200
