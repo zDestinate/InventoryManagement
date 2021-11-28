2e0fa34bbe54b8cdbe325d032c052f7f167997b2
zDestinate <EA.ADM1N.one@gmail.com>
2021-11-27 20:48:48 -0500

    Update console name

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -57,7 +57,7 @@ int main()
 	GetWindowThreadProcessId(hwndConsole, &dwProcessId);
 	if (GetCurrentProcessId() != dwProcessId)
 	{
-		SetConsoleTitle("InventoryManagement - Debug Console");
+		SetConsoleTitle("InventoryManagement - Developer Console");
 	}
 	else
 	{
